9bb8c51d69ef845662bffb2d33f330c2cc54ac48
Owen Sims <owen.sims@cyberlytic.com>
2018-10-07 21:56:00 +0100

    Finishing off initial Cambridge articles

--- src/extract_emails.py
+++ src/extract_emails.py
@@ -7,7 +7,6 @@ Created on Tue Oct  2 11:03:12 2018
 """
 
 import re
-import json
 import requests
 
 from bs4 import BeautifulSoup
@@ -22,20 +21,27 @@ JOURNAL_DICT = {
 ISSUE_LINK = "https://www.cambridge.org/core/journals/financial-history-review/core/journals/financial-history-review/issue/232EA68F292ECF1F1991E512C237C8DC"
 
 
-def get_journal_page(journal):
+def get_journal_pages(journal):
     url = '{}{}'.format(
         JOURNAL_DICT[journal],
         "/all-issues")
     request_page = requests.get(
         url=url,
         headers={'User-Agent': USER_AGENT})
+    hyperlinks = []
     journal_page_soup = BeautifulSoup(request_page.content, "lxml")
-    issue_hyperlinks = []
     for issue_block in journal_page_soup.find_all("li", {"class", "accordion-navigation"}):
         for word in str(issue_block).split():
             if '/issue/' in word:
-                cleaned_word = clean_hyperlink(hyperlink=word)
-                issue_hyperlinks.append(cleaned_word)
+                hyperlink = clean_hyperlink(hyperlink=word)
+                hyperlinks.append(hyperlink)
+    author_details = []
+    for h in hyperlinks:
+        article_details = get_article_details(
+            journal=journal,
+            issue_hyperlink=h)
+        author_details.append(article_details)
+    return author_details
 
 
 def get_article_details(journal, issue_hyperlink):
@@ -46,10 +52,16 @@ def get_article_details(journal, issue_hyperlink):
         url=url,
         headers={'User-Agent': USER_AGENT})
     issue_soup = BeautifulSoup(request_page.content, "lxml")
-    article_links = []
-    for issue_block in issue_soup.find_all("a", {"class", "part-link"}):
-        article_links.append(issue_block.attrs['href'])
-        
+    volume = issue_soup.find("span", {"class", "volume"}).get_text().strip().replace('\n', ' ')
+    author_details = {volume : []}
+    all_articles = issue_soup.find_all("a", {"class", "part-link"})
+    for issue_block in all_articles[:len(all_articles)-2]:
+        author_details[volume].append(
+            get_author_details(
+                journal=journal,
+                article_hyperlink=issue_block.attrs['href']))
+    return author_details
+
 
 def get_author_details(journal, article_hyperlink):
     url = '{}{}'.format(
@@ -59,23 +71,59 @@ def get_author_details(journal, article_hyperlink):
         url=url,
         headers={'User-Agent': USER_AGENT})
     article_soup = BeautifulSoup(request_page.content, "lxml")
-    title = article_soup.find("h1", {"class", "article-title"}).get_text()
-    doi_url = article_soup.find("a", {"class", "url doi"}).get_text()
-    authors = article_soup.find("a", {"class", "more-by-this-author "}).get_text()
-    abstract = article_soup.find("div", {"class", "abstract"}).get_text()
-    corr = article_soup.find("div", {"class", "corresp"}).get_text()
-    email = 'mailto:' in str(article_soup)
+    author_details = {
+        'title': attribute_check(article_soup, "h1", {"class", "article-title"}),
+        'doiUrl': attribute_check(article_soup, "a", {"class", "url doi"}),
+        'authors': attribute_check(article_soup, "a", {"class", "more-by-this-author "}),
+        'abstract': attribute_check(article_soup, "div", {"class", "abstract"}),
+        'corresp': attribute_check(article_soup, "div", {"class", "corresp"}),
+        'email': parse_email(article_soup=article_soup),
+        'institution': attribute_check(article_soup, "institution"),
+        'address': parse_address(soup=article_soup),
+        'hyperlink': url
+    }
+    return author_details
+
+
+def attribute_check(soup, tag, attrs=None):
+    if attrs is None:
+        raw_chunk = soup.find(tag)
+    else:
+        raw_chunk = soup.find(tag, attrs)
+    if raw_chunk is not None:
+        return raw_chunk.get_text().strip()
+    else:
+        return None
+
+
+def parse_address(soup):
+    address_array = soup.find_all("addrline")
+    address = soup.find("institution")
+    if address is None:
+        return None
+    else:
+        address = address.get_text() + ', '
+        for a in address_array:
+            address += a.get_text() + ' '
+        return address
 
 
 def parse_email(article_soup):
     article_str = str(article_soup)
     k = re.search('mailto:(.*)"', article_str)
-    return k.group(1)
-    
+    if k is None:
+        return None
+    else:
+        return k.group(1)
+
 
 def clean_hyperlink(hyperlink):
     link = hyperlink.split('"')[1]
     return link
-    
-    
-    
+
+def flatten_frame(author_details):
+    d = []
+    for k in author_details.keys():
+        for a in author_details[k]:
+            d.append(a)
+    return d
